c115160bc4f1bc208cf083aef9f328f346f0ddf1
Jiarui Wang <jiaruiwang@Jiaruis-MacBook-Air.local>
2024-01-21 18:21:51 -0500

    KVClient.newConnection

--- m1-stub/src/app_kvClient/KVClient.java
+++ m1-stub/src/app_kvClient/KVClient.java
@@ -1,11 +1,22 @@
 package app_kvClient;
 
 import client.KVCommInterface;
+import client.KVStore;
 
 public class KVClient implements IKVClient {
+
+    private KVStore kvStore = null;
+
     @Override
     public void newConnection(String hostname, int port) throws Exception{
-        // TODO Auto-generated method stub
+        kvStore = new KVStore(hostname, port);
+        try {
+            System.out.println("KVClient> Establishing Connection...");
+            kvStore.connect();
+            System.out.println("KVClient> Connection Established!");
+        } catch (Exception e) {
+            System.out.println("KVClient> Connection Establishment Failed!");
+        }
     }
 
     @Override
